2cf6a980ff3eda7eec7d1b8ef4c84d7720e599fc
Anoop <khinderanoop@gmail.com>
2020-01-29 11:02:45 +0000

    Updates

---
+++ precache-manifest.db980fcf9b0cbd4caffead363463921c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191328c172d67555834a6229e9b94365",
+    "url": "/anoopkaur/Portfolio/index.html"
+  },
+  {
+    "revision": "be22f45538662268f9c7",
+    "url": "/anoopkaur/Portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c4b022263685f87392a5",
+    "url": "/anoopkaur/Portfolio/static/css/main.e000abaa.chunk.css"
+  },
+  {
+    "revision": "be22f45538662268f9c7",
+    "url": "/anoopkaur/Portfolio/static/js/2.83092833.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/anoopkaur/Portfolio/static/js/2.83092833.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4b022263685f87392a5",
+    "url": "/anoopkaur/Portfolio/static/js/main.05b8bb4b.chunk.js"
+  },
+  {
+    "revision": "8caa71362b2cdd7fd8a0",
+    "url": "/anoopkaur/Portfolio/static/js/runtime-main.4caba571.js"
+  },
+  {
+    "revision": "d9c62910426bd36f1bec0918d4292e47",
+    "url": "/anoopkaur/Portfolio/static/media/Marriage.d9c62910.png"
+  },
+  {
+    "revision": "55e7d5d12d4b3ac691bfd5bf9afc8572",
+    "url": "/anoopkaur/Portfolio/static/media/blank2.55e7d5d1.jpg"
+  },
+  {
+    "revision": "06e3968ed8572bc3af94eaa364844c66",
+    "url": "/anoopkaur/Portfolio/static/media/cartoon1.06e3968e.jpg"
+  }
+]);
